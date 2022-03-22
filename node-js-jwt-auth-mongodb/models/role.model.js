93e6a7b84241ad3de3248fab628136484cebe8b3
ahlem-phantom <ahlem.laajili@esprit.tn>
2022-03-22 20:28:55 +0100

    jwt auth react nodejs mongodb expressjs

---
+++ node-js-jwt-auth-mongodb/models/role.model.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const Role = mongoose.model(
+  "Role",
+  new mongoose.Schema({
+    name: String
+  })
+);
+module.exports = Role;
---
+++ node-js-jwt-auth-mongodb/server.js
@@ -0,0 +1,79 @@
+
+const express = require("express");
+const cors = require("cors");
+const mongoose = require('mongoose');
+const app = express();
+
+const db = require("./models");
+const Role = db.role;
+require('dotenv').config();
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+  })
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+function initial() {
+    Role.estimatedDocumentCount((err, count) => {
+      if (!err && count === 0) {
+        new Role({
+          name: "user"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+          console.log("added 'user' to roles collection");
+        });
+        new Role({
+          name: "moderator"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+          console.log("added 'moderator' to roles collection");
+        });
+        new Role({
+          name: "admin"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+          console.log("added 'admin' to roles collection");
+        });
+      }
+    });
+  }
+
+  // routes
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);

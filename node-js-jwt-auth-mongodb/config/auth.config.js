49126b19f855258c9bd9fbd3d2149707adc0a594
Ahlem Laajili <78981558+ahlem-phantom@users.noreply.github.com>
2022-10-13 07:07:55 +0200

    updates

--- node-js-jwt-auth-mongodb/config/auth.config.js
+++ node-js-jwt-auth-mongodb/config/auth.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-    secret: "bezkoder-secret-key",
+    secret: "your-secret-key",
     jwtExpiration: 3600,           // 1 hour
     jwtRefreshExpiration: 86400,   // 24 hours
-    /* for test */
-    // jwtExpiration: 60,          // 1 minute
-    // jwtRefreshExpiration: 120,  // 2 minutes
   };
